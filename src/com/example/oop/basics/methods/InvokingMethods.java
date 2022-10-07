6e7dc46107d3359865776ea8f1ea8e7687d67135
Tomasz Krudysz <t.krudysz@gmail.com>
2022-10-07 21:27:38 +0200

    nowa klasa

---
+++ src/com/example/oop/basics/methods/InvokingMethods.java
@@ -0,0 +1,21 @@
+package com.example.oop.basics.methods;
+
+class MyMath {
+    public void add(int a, int b){
+        a = 20;
+        int result = a + b;
+        System.out.println("resoult: " + result);
+    }
+}
+/**
+ * Created by tkrud on 07.10.2022.
+ */
+public class InvokingMethods {
+    public static void main(String[] args) {
+        MyMath math = new MyMath();
+        int a = 10;
+        int b = 7;
+        math.add(a,b);
+        System.out.println(a);
+    }
+}
